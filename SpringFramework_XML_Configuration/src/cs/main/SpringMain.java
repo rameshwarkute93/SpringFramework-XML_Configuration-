cc5a9caaee66634e0bd026c29600937545c9ed01
Rameshwar Sarangdhar Kute <rameshwarkute93@gmail.com>
2026-02-04 11:25:49 +0530

    // Spring Framework XML Configuration

---
+++ SpringFramework_XML_Configuration/src/cs/main/SpringMain.java
@@ -0,0 +1,42 @@
+package cs.main;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import cs.model.Student;
+
+public class SpringMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String path = "cs/config/app.xml";
+		
+//		 With Spring
+		
+		ApplicationContext a = new ClassPathXmlApplicationContext(path);
+		Student s = (Student) a.getBean("s1");
+		s.display();
+
+// Spring is responsible for:
+//		✔ Creating object
+//		✔ Injecting values
+//		✔ Managing lifecycle
+//		✔ Reusing singleton objects
+		
+		// Without Spring
+//		Student s = new Student();
+//		s.setId(1);
+//		s.setName("Ram");
+//		s.setCity("Pimplaner");
+//		s.display();
+		
+//You are responsible for:
+
+//Creating object
+//Setting values
+//Managing lifecycle
+	
+
+	}
+
+}
